f7ce3319615e16afbdc5fd1f8c10868eff007828
Kevin Ross <kevin.ross@alienfast.com>
2022-08-08 15:16:18 -0500

    make storybook config typescript [skip ci]

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,10 @@
-module.exports = {
+
+const mainConfig = {
   stories: [
     "../src/stories/**/*.stories.mdx",
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["../preset.js", "@storybook/addon-essentials"],
-};
+}
+
+export default mainConfig
