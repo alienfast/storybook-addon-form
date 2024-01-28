f2d540a7e017b72caca36cc44333b3c6b860a318
Kevin Ross <kevin.ross@alienfast.com>
2024-01-28 10:03:09 -0600

    Workaround TabsState bug https://github.com/storybookjs/storybook/issues/25308

--- src/PanelContent.tsx
+++ src/PanelContent.tsx
@@ -1,4 +1,4 @@
-import { Placeholder, TabsState } from '@storybook/components'
+import { TabsState } from '@storybook/components'
 import { convert, themes } from '@storybook/theming'
 import * as React from 'react'
 
@@ -16,7 +16,8 @@ export const PanelContent: React.FC<PanelProps> = (props: PanelProps) => {
   const { id, state } = props.results || {}
   const { errors, initialValues, values, ...restState } = state || {}
 
-  const info = id ? ` (${id})` : ''
+  // const info = id ? ` (${id})` : ''
+
   // console.log(
   //   'render',
   //   'id:',
@@ -30,66 +31,35 @@ export const PanelContent: React.FC<PanelProps> = (props: PanelProps) => {
   //   'restState:',
   //   restState,
   // )
+
+  // rerender bug with TabsState so hack in a key https://github.com/storybookjs/storybook/issues/25308
+  // this hack refocuses on values tab, but it's better than the alternative
+  const key = String(new Date().getTime())
   return (
     <>
-      <div>
-        hack 1 until the storybook up Tabs component is fixed and allows for updates:
-        <DisplayJson o={values || {}} />
-      </div>
+      {/* <div>{info}</div> */}
 
-      <TabsState initial="values" backgroundColor={convert(themes.normal).background.hoverable}>
+      <TabsState
+        initial="values"
+        backgroundColor={convert(themes.normal).background.hoverable}
+        key={key}
+      >
         <div id="values" title="Values" color={convert(themes.normal).color.secondary}>
-          {({ active, selected }: { active: boolean; selected: string }) =>
-            active ? (
-              <div>
-                <div>{selected} is selected</div>
-                <div>
-                  hack 2<div>raw: {String(values)}</div>
-                  <DisplayJson o={values || {}} />
-                </div>
-              </div>
-            ) : (
-              <div>not selected</div>
-            )
-          }
-          <div>
-            hack 2<div>raw: {String(values)}</div>
-            <DisplayJson o={values || {}} />
-          </div>
-          <Placeholder>
-            <>{info}</>
-            <DisplayJson o={values || {}} />
-          </Placeholder>
+          <DisplayJson o={values || {}} />
         </div>
-        <div id="errors" title={`Errors`} color={convert(themes.normal).color.negative}>
-          <Placeholder>
-            <>{info}</>
-            <DisplayJson o={errors || {}} />
-          </Placeholder>
+        <div id="errors" title="Errors" color={convert(themes.normal).color.negative}>
+          <DisplayJson o={errors || {}} />
         </div>
         <div
           id="initialValues"
           title="Initial Values"
           color={convert(themes.normal).color.secondary}
         >
-          <Placeholder>
-            <>{info}</>
-            <DisplayJson o={initialValues || {}} />
-          </Placeholder>
+          <DisplayJson o={initialValues || {}} />
         </div>
-        <div id="state" title="State" color={convert(themes.normal).color.secondary}>
-          <Placeholder>
-            <>Rest of form state{info}</>
-            <DisplayJson o={restState || {}} />
-          </Placeholder>
+        <div id="state" title="Other state" color={convert(themes.normal).color.secondary}>
+          <DisplayJson o={restState || {}} />
         </div>
-        {/* <div
-          id="warning"
-          title={`${results.warning.length} Warning`}
-          color={convert(themes.normal).color.warning}
-        >
-          <List items={results.warning} />
-        </div> */}
       </TabsState>
     </>
   )
