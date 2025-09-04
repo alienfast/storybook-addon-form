fbe469317e7edb63099b01f278c8ba1fb87569e6
Kevin Ross <kevin.ross@alienfast.com>
2025-09-04 16:46:58 -0500

    update to react 19

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   packageFile: './package.json',
   packageManager: 'yarn',
-  reject: ['react', 'react-dom', '@types/react', '@types/react-dom'],
+  // reject: ['react', 'react-dom', '@types/react', '@types/react-dom'],
   // target: (name: string, versionRange: SemVer[]) => string
-  target: (name) =>
-    // name === 'yup' ? '@next' : name.startsWith('@storybook/') ? '@next' : 'latest',
-    name === 'yup' ? '@next' : 'latest',
+  // target: (name) =>
+  //   // name === 'yup' ? '@next' : name.startsWith('@storybook/') ? '@next' : 'latest',
+  //   name === 'yup' ? '@next' : 'latest',
 }
